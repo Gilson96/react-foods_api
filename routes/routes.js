80275b124bd5a35774454bd8d72118bf3ea305ea
Gilson96 <grafael99@gmail.com>
2024-07-21 21:58:56 +0100

    Routes fixed

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const operations = require("../controllers/controllers");
 const router = express.Router();
 
-router.get('/', operations.getFood);
-router.get('/:id', operations.getFoods);
+router.get('/', operations.getFoods);
+router.get('/:id', operations.getFood);
 router.post('/', operations.createFood);
 router.put('/:id', operations.updateFood);
-router.delete('/', operations.deleteFood);
+router.delete('/:id', operations.deleteFood);
 
 router.post('/:id/person', operations.createPerson);
 router.get('/:id/person', operations.getPerson);
