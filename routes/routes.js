84a1fd9c5b4d5c14e58e2310c59928ff6600ad5b
Gilson96 <grafael99@gmail.com>
2025-05-05 22:27:29 +0100

    Added new routes for user

--- routes/routes.js
+++ routes/routes.js
@@ -58,8 +58,8 @@ router.post('/signup', [
 router.post('/login', userOperations.login)
 router.use(checkAuth)
 router.get('/user', userOperations.getUsers)
-router.post(':userId', userOperations.AddFavouriteRestaurants)
-router.post(':userId', userOperations.AddOrders)
+router.post(':userId/favourites', userOperations.AddFavouriteRestaurants)
+router.post(':userId/orders', userOperations.AddOrders)
 router.delete('/user/delete', userOperations.UserDelete)
 
 module.exports = router;
