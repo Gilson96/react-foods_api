206d0a189a6f1a181a412dbfdbd7a4a61d914445
Gilson96 <grafael99@gmail.com>
2025-05-07 20:08:29 +0100

    Added stripe

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -4,10 +4,11 @@ dotenv.config({ path: __dirname + '/.env' });
 const stripe = require('stripe')('sk_test_51RM9WqQc9p8NGwhTVWaT5PLExXNFPJBerqfEWc6TrtKhhQK7WBNtG0iSSs4MlRZWAGqTsimEecW1AF3cn39RGIMm00YDaNWQfJ');
 
 exports.create_payment_intent = async (req, res) => {
+    const { totalPrice } = req.body
 
     try {
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: req.body,
+            amount: totalPrice,
             currency: 'gbp',
         });
         res.status(200).json({
@@ -17,5 +18,5 @@ exports.create_payment_intent = async (req, res) => {
         res.status(500).json({ message: 'Stripe connection error' });
     }
 
-    console.log()
+  
 }
