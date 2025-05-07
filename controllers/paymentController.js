45628c39796174aee868e56b4a8de270d8e326e0
Gilson96 <grafael99@gmail.com>
2025-05-07 19:56:08 +0100

    Added stripe

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -4,11 +4,10 @@ dotenv.config({ path: __dirname + '/.env' });
 const stripe = require('stripe')('sk_test_51RM9WqQc9p8NGwhTVWaT5PLExXNFPJBerqfEWc6TrtKhhQK7WBNtG0iSSs4MlRZWAGqTsimEecW1AF3cn39RGIMm00YDaNWQfJ');
 
 exports.create_payment_intent = async (req, res) => {
-    const { total } = req.body
 
     try {
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: total,
+            amount: req.body,
             currency: 'gbp',
         });
         res.status(200).json({
