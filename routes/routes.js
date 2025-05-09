df49207ed1c5cd198f3a82e35959ff76ddb24a95
Gilson96 <grafael99@gmail.com>
2025-05-09 16:52:18 +0100

    Fixing stripe

--- routes/routes.js
+++ routes/routes.js
@@ -38,7 +38,7 @@ router.put('/:restaurantId/reviews/:reviewsId', ratingAndReviewsOperations.updat
 router.delete('/:restaurantId/reviews/:reviewsId', ratingAndReviewsOperations.deleteRatingAndReview);
 
 // payement
-router.post('/create-checkout-session', paymentOperations.create_payment_intent);
+router.post('/create-checkout-session', paymentOperations.CreateCheckoutSession);
 
 
 router.post('/signup', [
