d69984368d20e88782f31d2e73c690815d36ff5b
Gilson96 <grafael99@gmail.com>
2025-07-31 15:26:50 +0100

    set up upload image to cloud

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,6 +10,7 @@ import userOperations from "../controllers/userController";
 import categoryOperations from "../controllers/categoryController";
 import paymentOperations from "../controllers/paymentController";
 import checkAuth from "../middleware/check-auth";
+import crypto from "crypto";
 
 const router = express.Router();
 
@@ -129,5 +130,20 @@ router.post(
   foodOperations.createFood as RequestHandler
 );
 router.delete("/:userId/delete", userOperations.UserDelete);
+// image upload route
+router.get("/auth", (req, res) => {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const token = crypto.randomBytes(16).toString("hex");
 
+  const signature = crypto
+    .createHmac("sha1", process.env.IMAGEKIT_PRIVATE_KEY!)
+    .update(token + timestamp)
+    .digest("hex");
+
+  res.json({
+    token,
+    expire: timestamp + 240,
+    signature,
+  });
+});
 export default router;
