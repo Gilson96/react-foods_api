4e4b27b813ae947ae686683776429b56bd984829
Gilson96 <grafael99@gmail.com>
2025-08-07 17:10:58 +0100

    Added new origin domain

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const allowedOrigins =
 // - credentials: true allows cookies (HttpOnly JWT) to be sent
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://gilson96.github.io/mern-food",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true // Required for cookie-based authentication
