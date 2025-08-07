7c98ca21b19963f6fc70726161526ec9c9dce45e
Gilson96 <grafael99@gmail.com>
2025-08-07 10:50:32 +0100

    Fixed CORS

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const allowedOrigins =
 // - credentials: true allows cookies (HttpOnly JWT) to be sent
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true // Required for cookie-based authentication
