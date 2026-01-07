5289395299796a51c07235a1fd3397c16ce4d077
Gilson96 <grafael99@gmail.com>
2026-01-07 14:04:17 +0000

    Fix cors

--- src/server.ts
+++ src/server.ts
@@ -5,32 +5,15 @@ import connectDB from "./dbConnection";
 import mongoose from "mongoose";
 import path from "path";
 import routes from "./routes/routes";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import helmet from "helmet"
 
-// Load environment variables from .env file
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
-// Connect to MongoDB
 connectDB();
 
 const app: Application = express();
 
-// Define allowed origins based on environment
-const allowedOrigins =
-  process.env.NODE_ENV === "development"
-    ? "https://mernfoods.netlify.app"
-    : "http://localhost:5173";
-
-app.use(cors({ origin: "https://mernfoods.netlify.app" }));
+app.use(cors());
 app.use(express.json());
-
-// Parse cookies from incoming requests
-app.use(cookieParser());
-
-// Enable GZIP compression for responses
-app.use(compression());
 
 app.use("/", routes);
 
